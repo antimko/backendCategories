dd89514e8308ca69422c87806e0f29d99bf735fd
Andrejus Timko <timcostudios@gmail.com>
2021-07-07 13:10:40 +0300

    sd

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const mongoose = require('mongoose');
 const { DataBase2 } = require('./config.js');
 const Post = require('./models/post');
 
+mongoose
+  .connect(mongoDbString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('Conneced to my Mongoose');
+    app.listen(4000);
+  })
+  .catch((err) => console.error(err.message));
+
 app.get('/', function (req, res) {
   res.send('Serveris veikia');
 });
